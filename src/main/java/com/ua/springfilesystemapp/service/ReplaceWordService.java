57c294de6cbfcfdf26fa0d0165287a439c046aae
AlexeNovokhatko <novohatko2000l@gmail.com>
2020-08-25 16:19:52 +0300

    Added replace logic for file name

--- src/main/java/com/ua/springfilesystemapp/service/ReplaceWordService.java
+++ src/main/java/com/ua/springfilesystemapp/service/ReplaceWordService.java
@@ -3,13 +3,12 @@ package com.ua.springfilesystemapp.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -18,18 +17,32 @@ import java.util.stream.Stream;
 public class ReplaceWordService {
     @Autowired
     ReadFilesPathService readFilesPathService;
+
     public void updateAllFiles(String startFilePath, String oldWord, String newWord) throws IOException {
-        List<String> filePathes = readFilesPathService.readFilesPath(startFilePath);
-        for (int i =0; i<filePathes.size();i++) {
-            updateFile(filePathes.get(i),oldWord,newWord);
+        List<String> allFiles = readFilesPathService.scanAllSubfolders(startFilePath);
+        for(int i =0; i<allFiles.size();i++) {
+            makeReplacement(allFiles.get(i), oldWord, newWord);
+            renameIfNeeded(allFiles.get(i), oldWord, newWord);
         }
     }
 
-    public void updateFile(String filePath, String oldWord, String newWord) throws IOException {
+    private void makeReplacement(String filePath, String oldWord, String newWord) throws IOException {
         Path path = Paths.get(filePath);
         Stream<String> lines = Files.lines(path);
         List<String> replaced = lines.map(line -> line.replaceAll(oldWord, newWord)).collect(Collectors.toList());
         Files.write(path, replaced);
         lines.close();
     }
+
+    private void renameIfNeeded(String fileName, String oldWord, String newWord) {
+        // TODO: 25.08.2020 rename directory
+       File oldFile = new File(fileName);
+       String newFileName = fileName.replaceAll(oldWord,newWord);
+       File newFile = new File(newFileName);
+        if (oldFile.renameTo(newFile)){
+           System.out.println("File Renamed successfully");
+       }
+    }
+
+
 }
